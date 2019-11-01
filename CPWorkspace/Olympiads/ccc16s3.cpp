1d18c05725c79facb768dce23e18219f096bf9bc
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-11-01 10:30:40 -0400

    ccc16s3 working

--- CPWorkspace/Olympiads/ccc16s3.cpp
+++ CPWorkspace/Olympiads/ccc16s3.cpp
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include <vector>
+#include <queue>
 using namespace std;
-int n, m;
-vector<vector<int> > adj(1);
-bool pho[100005];
-int main(){
+int n, m, s;
+vector<vector<int>> adj(1);
+vector<int> pho;
+queue<int> op;
+int main()
+{
     scanf("%d %d", &n, &m);
     adj.resize(n);
-    for(int i=0;i<m;i++){
-        pho[m] = true;
+    for (int i = 0, p; i < m; i++)
+    {
+        scanf("%d ", &p);
+        pho.push_back(p);
     }
-    for(int i=0, a, b;i<n;i++){
+    for (int i = 0, a, b; i < n; i++)
+    {
         scanf("%d %d", &a, &b);
         adj[a].push_back(b);
         adj[b].push_back(a);
-        
     }
+    for (int i = 0; i < m; i++)
+    {
+        if (adj[pho[i]].size() == 1)
+        {
+            s = pho[i];
+        }
+    }
+    op.push(s);
+    while(!pho.empty()){
 
+    }
 
 }
