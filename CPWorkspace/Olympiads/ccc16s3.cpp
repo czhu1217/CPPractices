051ddb9fd851543a4f497f2b7fe30b5746ac6e96
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-11-03 11:21:11 -0500

    16s3

--- CPWorkspace/Olympiads/ccc16s3.cpp
+++ CPWorkspace/Olympiads/ccc16s3.cpp
@@ -1,36 +1,64 @@
 #include <stdio.h>
 #include <vector>
 #include <queue>
+#include <map>
 using namespace std;
-int n, m, s;
+int n, m, s, si, cnt, lv[100005], step = 0;
 vector<vector<int>> adj(1);
-vector<int> pho;
-queue<int> op;
+map<int, bool> pho;
+queue<int> op, path;
+void clear( std::queue<int> &q )
+{
+   std::queue<int> empty;
+   std::swap( q, empty );
+}
 int main()
 {
     scanf("%d %d", &n, &m);
     adj.resize(n);
+    cnt = m;
     for (int i = 0, p; i < m; i++)
     {
         scanf("%d ", &p);
-        pho.push_back(p);
+        pho[p] = true;
     }
-    for (int i = 0, a, b; i < n; i++)
+    for (int i = 0, a, b; i < n-1; i++)
     {
         scanf("%d %d", &a, &b);
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (adj[pho[i]].size() == 1)
+        if (pho[i]&&adj[i].size() == 1)
         {
-            s = pho[i];
+            s = i;
+
         }
     }
-    op.push(s);
-    while(!pho.empty()){
+    path.push(s);
+    lv[s] = 0;
+    while(cnt!=0){
 
+        int t = path.front();
+        if(pho[t]==true){
+            pho[t] = false;
+            cnt--;
+            step += lv[t];
+            lv[t] = 0;
+            clear(path);
+            path.push(t);
+
+            
+        }
+        path.pop(); 
+        for(int i=0;i<adj[t].size();i++){
+            path.push(adj[t][i]);
+            lv[adj[t][i]] = lv[t]+1;
+        }
     }
+
+    printf("%d\n", step);
+    return 0;
 
 }
