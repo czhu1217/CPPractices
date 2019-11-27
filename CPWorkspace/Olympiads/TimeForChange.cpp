b784310ecde7059a31683d879a8eb61b0bba9b96
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-11-27 15:53:47 -0500

    one more

---
+++ CPWorkspace/Olympiads/TimeForChange.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+using namespace std;
+int M, N, c[11], ar[105];
+int main(){
+    for(int i=0;i<5;i++){
+        scanf("%d %d", &M, &N);
+    for(int i=0;i<N;i++){
+        scanf("%d ", &c[i]);
+    }
+    for(int i=0;i<N;i++){
+        for(int i=0;i<=M;i++){
+            
+        }
+    }
+    }
+    
+   
+
+
+}
