373822ccf67d0cfb5a0c6fe26d3214590bee2866
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-12-09 00:21:51 -0500

    max undefined

--- CPWorkspace/Olympiads/HolyGrailWar.cpp
+++ CPWorkspace/Olympiads/HolyGrailWar.cpp
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <iostream>
-#include <algorithm>
-using namespace std;
-typedef long long int lld;
-const int MM = 1e5+5;
-int N, Q;
-int a[MM];
 
+#include <algorithm>
+
+const int MM = 100002;
+#define lc id<<1
+#define rc id<<1|1
+typedef long long int lld;
 struct node{
     int l, r; lld pre, suf, ans, sum;
 } seg[3*MM];
 
+int N, Q;
+char op;
+
+
 void pushup(int id){
-    seg[id].pre = max(seg[id*2].pre, seg[id*2].sum+seg[id*2+1].pre);
-    seg[id].suf = max(seg[id*2].suf+seg[id*2+1].sum, seg[id*2].sum+seg[id*2+1].pre);
+    seg[id].pre = max(seg[lc].pre, seg[lc].sum+seg[rc].pre);
+    seg[id].suf = max(seg[lc].suf+seg[id*2+1].sum, seg[rc].suf);
     seg[id].ans = max(max(seg[2*id].ans, seg[2*id+1].ans), seg[2*id].suf+seg[2*id+1].pre);
     seg[id].sum = seg[id*2].sum+seg[2*id+1].sum;
 }
@@ -21,10 +25,8 @@ void build(int l, int r, int id){
     seg[id].l = l;
     seg[id].r  =r;
     if(l==r){
-        seg[id].ans = a[id];
-        seg[id].pre = a[id];
-        seg[id].suf = a[id];
-        seg[id].sum = a[id];
+        scanf("%lld", &seg[id].pre);
+        seg[id].suf = seg[id].sum = seg[id].ans = seg[id].pre;
         return;
     }
     int mid = (seg[id].l + seg[id].r)/2;
@@ -35,7 +37,7 @@ void build(int l, int r, int id){
 
 }
 node query(int l, int r, int id){
-    if(seg[id].l==l&&seg[id].r==r){
+    if(seg[id].l==l&&seg[id].r==r)return seg[id];
         int mid = (seg[id].l + seg[id].r)/2;
         if(r<=mid){
             return query(l, r, 2*id);
@@ -52,11 +54,10 @@ node query(int l, int r, int id){
             return ret;
 
         }
-    }
 
 }
 void update(int pos, int val, int id){
-    if(seg[id].l==seg[id].r){
+    if(seg[id].l==seg[id].r&&seg[id].l==pos){
         seg[id].pre=seg[id].suf=seg[id].ans = seg[id].sum = val;
         return;
     }
@@ -71,23 +72,19 @@ void update(int pos, int val, int id){
 
 }
 int main(){
-    cin.tie(0);cin.sync_with_stdio(0);
-    cin >> N >> Q;
-    for(int i=1;i<=N;i++){
-        cin >> a[i];
-    }
-    build(1, N, 1);
-        char c;
+    scanf("%d %d", &N, &Q);
+     build(1, N, 1);
+
     for(int i=0, x, y;i<Q;i++){
 
-        cin >> c >> x >> y;
+        scanf(" %c %d %d", &op, &x, &y);
 
-        if(c=='Q'){
-            cout << query(x, y, 1).ans;
+        if(op=='Q'){
+            printf("%lld\n", query(x, y, 1).ans);
         }
         else{
             update(x, y, 1);
-            cout << "debug";
+            
         }
     }
     return 0;
