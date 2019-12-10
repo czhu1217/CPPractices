a430f77773ee39da1bbcd38379a25960d6f2f0a1
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-12-10 14:50:42 -0500

    don't die

--- CPWorkspace/Olympiads/LetterFrequency.cpp
+++ CPWorkspace/Olympiads/LetterFrequency.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <map>
+#include <vector>
 using namespace std;
 const int MM = 1e6+5;
-int a[26][MM], Q;
+int pre[26][MM], Q;
+map<char, vector
 string s;
 int main(){
     getline(cin, s);
-    
+    for(int i=0;i<s.length();i++){
+
+    }
 }
