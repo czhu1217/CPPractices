6ae71591383128da31a57d61f45c54a40a0cec4d
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-12-12 23:33:25 -0500

    i tried

---
+++ CPWorkspace/Olympiads/Stol.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int r, c, cl, cr, cs, ms, mr, ml, mu, md;
+bool b[402][402];
+map<pair<int, int>, bool> m;
+int start=0, op[402], sum=0;
+char ch;
+int main(){
+    cin >> r >> c ;
+    for(int i=0;i<r;i++){
+        for(int c = 0;i<c;i++){
+            cin >> ch;
+            if(c=='.'){
+                b[r][c] = true;
+            }else b[r][c] = false;
+    
+        }
+    }
+    for(cl=0;cl<c;cl++){
+        for(cr=0;cr<c;cr++){
+            for(int i=0;i<r;i++){
+                
+            }
+        }
+    }
+    
+
+}
