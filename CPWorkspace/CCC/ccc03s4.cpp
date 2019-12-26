4c98fd6b07ab1b7bb70688f26ce4a1954f1a509a
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-12-26 13:06:38 -0500

    made it a bit more easy to read (I should rly work my naming conventions

--- CPWorkspace/CCC/ccc03s4.cpp
+++ CPWorkspace/CCC/ccc03s4.cpp
@@ -9,17 +9,10 @@ int N;
 vector<string> suf;
 int lcpp(string s, string t)
 {
-    // cout << "lcp" << " " + s + " " + t;
     int n = min(s.size(), t.size());
     for (int i = 0; i < n; i++)
-    {
-        if (s[i] != t[i])
-        {
-            // cout << i << "\n";
+        if (s[i] != t[i])   
             return i;
-        }
-    }
-    // cout << n << "\n";
     return n;
 }
 int main()
@@ -34,27 +27,13 @@ int main()
             cout << 0 << "\n";
             break;
         }
-        for (int j = 0; j < s.size(); j++)
-        {
-            // cout << "loop";
-            suf.push_back(s.substr(j));
-            // cout << s.substr(j) + "\n";
-        }
+        for (int j = 0; j < s.size(); j++) suf.push_back(s.substr(j));
         sort(suf.begin(), suf.end());
         int cnt = suf.front().size();
         for(int i=1;i<suf.size();i++){
-            // cout << "cnt " << cnt << "\n";
             int lcp = lcpp(suf.at(i), suf.at(i-1));
             cnt += suf.at(i).size() - lcp;
         }
-
-        // for (std::vector<string>::iterator it = suf.begin() + 1; it != suf.end(); it++)
-        // {
-          
-        //     int lcp = lcpp(*it, *(it - 1));
-        //     string temp = *it;
-        //     cnt += temp.size() - lcp;
-        // }
         cout << cnt+1 << "\n";
     }
     return 0;
