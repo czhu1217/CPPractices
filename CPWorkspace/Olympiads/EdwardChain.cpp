3e2d890d2e66f55ec00d960c9fa162577a664393
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2019-12-30 01:11:45 -0500

    kms

---
+++ CPWorkspace/Olympiads/EdwardChain.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int N;
+char a[1000005], preg, pres, sufg, sufs;
+long long asns = 0;
+int main(){
+    scanf("%d", &N);
+    scanf(" %c", &a[0]);
+    for(int i=1;i<N;i++){
+        scanf(" %c", &a[i]);
+    }
+    
+    return 0;
+
+}
