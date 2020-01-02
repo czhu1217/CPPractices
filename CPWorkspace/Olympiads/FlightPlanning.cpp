c5257089efa9106be1451a41dd7c605d1add71e6
cindyzhu0830 <45746066+cindyzhu0830@users.noreply.github.com>
2020-01-01 21:25:51 -0500

    idk

--- CPWorkspace/Olympiads/FlightPlanning.cpp
+++ CPWorkspace/Olympiads/FlightPlanning.cpp
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <vector>
+
 using namespace std;
 
 struct flight{  int u, v, d;} f[20005];
 int n, m, k, q;
+bool hub[20005];
+vector<vector<pair<int, int>>> adj(1);
 
 int main(){
     scanf("%d %d %d %d", &n, &m, &k, &q);
-    for(int i=0;i<n;i++){
+    adj.resize(n+1);
+    for(int i=0;i<m;i++){
         scanf("%d %d %d", &f[i].u, &f[i].v, &f[i].d);
+        adj[f[i].u].push_back({f[i].v, f[i].d});
+    }
+    for(int i=0;i<k;i++){
+        int temp;
+        scanf("%d", &temp);
+        hub[temp]=true;
+    }
+    for(int i=0;i<q;i++){
+
     }
 
 
