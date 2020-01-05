bc8de8921ea61b9b59badf93a5fa2ba5c6f0f466
Cindy Zhu <cindyzhu0830@gmail.com>
2020-01-05 12:23:41 -0500

    c2p1

--- DMOPC/19c2p0.cpp
+++ DMOPC/19c2p0.cpp
@@ -1,1 +1,15 @@
-//test
+//roller coaster
+#include <stdio.h>
+int N, mx, mi,  cnt;
+int main(){
+    scanf("%d %d %d", &N, &mi, &mx) ;
+    cnt = N;
+    for(int i=0;i<N;i++){
+        int t;
+        scanf("%d", &t);
+        if(t<mi||t>mx) cnt--;
+
+    }
+    printf("%d\n", cnt);
+    return 0;
+}
