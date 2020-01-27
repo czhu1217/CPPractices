348059f06b6a1a9711ac985ccd6ebbcd99fdf12a
Cindy Zhu <cindyzhu0830@gmail.com>
2020-01-27 13:20:48 -0500

    ccc14s3

---
+++ CCC/ccc14s3.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+using namespace std;
+int T, N, a[100005];
+bool flag;
+vector<int> river, branch;
+int main()
+{
+
+    cin >> T;
+    river.push_back(0);
+    for (int i = 0; i < T; i++)
+    {
+        river.clear();
+        branch.clear();
+        flag = false;
+        memset(a, 0, sizeof(a));
+        cin >> N;
+        for (int j = N-1; j >= 0; j--)
+        {
+            cin >> a[j];
+        }
+        for (int j = 0, x; j < N; j++)
+        {
+            x = a[j];
+            if (river.back() == x - 1 || (river.empty() && x == 1))
+            {
+                river.push_back(x);
+                // cout << "push in river" << x << endl;
+                while (true)
+                {
+                    if (branch.back() == river.back() + 1)
+                    {
+                        int t = branch.back();
+                        branch.pop_back();
+                        river.push_back(t);
+                        // cout << "push in river" << x << endl;
+                    }
+                    else
+                        break;
+                }
+            }
+            else
+            {
+                if (branch.empty() || x < branch.back())
+                {
+                    branch.push_back(x);
+                    // cout << "push in branch" << x << endl;
+                }
+                else
+                {
+                    cout << "N\n";
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if ((!flag) && branch.empty())
+            cout << "Y\n";
+    }
+    return 0;
+}
