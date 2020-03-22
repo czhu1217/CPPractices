2bab5f109fb9adf6ad41e1b51611f975685c49a9
Cindy Zhu <cindyzhu0830@gmail.com>
2020-03-22 15:53:29 -0400

    dmopc

--- a-todo/19c5p4.cpp
+++ a-todo/19c5p4.cpp
@@ -14,22 +14,52 @@ typedef long long ll;
 #define f first
 #define s second
 using namespace std;
-const int MM = 1e5+2;
-pair<int, int> qu[MM];
-int n, q, m[MM], v[MM];
-int main(){
+const int MM = 1e5 + 2;
+vector<pair<int, int>> pi;
+int n, q, m[MM], v[MM], a[MM], query[MM];
+int find(int idx)
+{
+    if (a[idx]!=0)
+        return a[idx];
+    if (v[idx] == 0)
+    {
+        return m[idx];
+    }
+    return m[idx] + find(v[idx]);
+}
+int main()
+{
     cin >> n >> q;
-    for(int i=1;i<=n;i++){
+    memset(a, 0, sizeof(a));
+    for (int i = 1; i <= n; i++)
+    {
         cin >> m[i];
     }
-    for(int i=1;i<=n;i++){
+    for (int i = 1; i <= n; i++)
+    {
         cin >> v[i];
     }
-    for(int i=1;i<=q;i++){
-        cin >> qu[i].f >> qu[i].s;
+    for (int i = 1, a, b; i <= q; i++)
+    {
+        cin >>a >> b;
+        query[i] = b - a;
     }
+    for (int i = 1; i <= n; i++)
+        if (a[i]==0)  a[i] = find(i);
     
-
-
-
+    for(int i=1;i<=q;i++){
+        if(query[i]<=0){
+                cout << -1 << endl;
+                continue;
+            }
+        int ans=-1;
+        for(int j=1;j<=n;j++){
+            if(a[j]>=query[i]){
+                ans = j;
+                break;
+            }
+        }
+        cout << ans << "\n";
+    }
+    return 0;
 }
