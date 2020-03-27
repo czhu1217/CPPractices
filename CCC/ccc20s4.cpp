df8f9c1143870f4c22ed73bf6a6490c5d58c398e
Cindy Zhu <cindyzhu0830@gmail.com>
2020-03-26 21:00:59 -0400

    s4

--- CCC/ccc20s4.cpp
+++ CCC/ccc20s4.cpp
@@ -12,8 +12,13 @@ typedef long long ll;
 const int MM = 1e6+2;
 int n, a[MM], b[MM], c[MM];
 string s;
+int ans = 1e9+1;
 void update(int x[], int y[], int i){
-    int nx = x[n], ny = y[n];
+    int nx = x[n], ny = y[n], swap = 0;
+    swap += nx - (x[i]-x[i-nx]);
+    swap += ny - (y[i-nx]-y[i-nx-ny]);
+    swap -= min(y[i]-y[i-nx], x[i-nx]-x[i-nx-ny]);
+    ans = min(ans, swap);
     
     }
 int main(){
@@ -21,8 +26,8 @@ int main(){
     for(int i=1;i<=n;i++){
         a[i] = a[i-1]; b[i] = b[i-1]; c[i] = c[i-1];
         if(s[i-1]=='A') a[i]++;
-        else if (a[i-1]=='B')b[i]++;
-        else a[i]++;
+        else if (s[i-1]=='B')b[i]++;
+        else c[i]++;
 
     }
     int na = a[n], nb = b[n], nc = c[n];
@@ -38,5 +43,7 @@ int main(){
             update(b, c, i); update(c, b, i);
         }
     }
+    cout << ans << "\n";
+    return 0;
 
 }
