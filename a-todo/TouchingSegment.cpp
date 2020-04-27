1d842f855ef7579a9531b5bedf0d9dd28ca99aef
Cindy Zhu <cindyzhu0830@gmail.com>
2020-04-27 00:09:26 -0400

    touching segments finally sjdakjhkhjd

--- a-todo/TouchingSegment.cpp
+++ a-todo/TouchingSegment.cpp
@@ -6,39 +6,32 @@
 #include <cstring>
 
 using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pi;
+typedef pair<int, int> pi;
 #define f first
 #define s second
-const ll MM = 2e5+5;
-struct node{ll l, r, val, lz;} seg[3*MM];
-ll T, N; pi a[MM];
-struct event{
-    ll l, i, v;
-    bool operator<(const event &e){
-        return l<e.l;
-    }
-};
-void build(ll l, ll r, ll rt){
+const int MM = 1e5+5;
+struct node{int l, r, val, lz;} seg[6*MM];
+int T, N; pi a[MM]; vector<int> lft[2*MM], rit[2*MM], p;
+void build(int l, int r, int rt){
     seg[rt].l=l; seg[rt].r = r;
-    if(l==r)return;
+    if(l==r){lft[l].clear(); rit[l].clear(); return;}
 
-    ll mid = (seg[rt].l+seg[rt].r)/2;
+    int mid = (l+r)/2;
     build(l, mid, 2*rt); build(mid+1, r, 2*rt+1);
 
 
 }
-void push_down(ll rt){
+void push_down(int rt){
     seg[2*rt].lz += seg[rt].lz; seg[2*rt+1].lz += seg[rt].lz;
     seg[2*rt].val += seg[rt].lz; seg[2*rt+1].val += seg[rt].lz;
     seg[rt].lz=0;
 }
-void update(ll l, ll r, ll v, ll rt){
+void update(int l, int r, int v, int rt){
     if(seg[rt].l==l&&seg[rt].r==r){
         seg[rt].val+=v; seg[rt].lz+=v;return;
     }
     if(seg[rt].lz)push_down(rt);
-    ll mid = (seg[rt].l+seg[rt].r)/2;
+    int mid = (seg[rt].l+seg[rt].r)/2;
     if(r<=mid)update(l, r, v, 2*rt);
     else if(l>mid)update(l, r, v, 2*rt+1);
     else{
@@ -49,37 +42,30 @@ void update(ll l, ll r, ll v, ll rt){
 }
 int main(){
     cin >> T;
-    for(ll t=1;t<=T;t++){
-        map<ll, ll> mp; vector<event> p;
-        memset(seg, 0, sizeof seg);
-        cin >> N; ll ans=0, cnt=0, line=0;
-        for(ll i=1;i<=N;i++){
-            scanf("%lld %lld", &a[i].f, &a[i].s);
-            mp[a[i].f]=0; mp[a[i].s] = 0;
+    for(int t=1;t<=T;t++){
+        memset(seg, 0, sizeof seg);p.clear();
+        scanf("%d", &N); int ans=0, cnt=0, line=0;
+        for(int i=0; i<N; i++){
+            scanf("%d %d", &a[i].f, &a[i].s);
+            p.push_back(a[i].f); p.push_back(a[i].s);
         }
-        for(auto it=mp.begin();it!=mp.end();it++){
-            it->s = ++cnt;
+        sort(p.begin(), p.end()); p.resize(unique(p.begin(), p.end()) - p.begin());
+        build(0, p.size()-1, 1);
+        for(int i=0; i<N; i++){
+            a[i].f = lower_bound(p.begin(), p.end(), a[i].f) - p.begin();
+            a[i].s = lower_bound(p.begin(), p.end(), a[i].s) - p.begin();
+            update(a[i].f, a[i].s, 1, 1);lft[a[i].f].push_back(i); rit[a[i].s].push_back(i);
         }
-        build(1, cnt, 1);
-        for(ll i=1;i<=N;i++){
-            update(mp[a[i].f], mp[a[i].s], 1, 1);
-            p.push_back({mp[a[i].f],i,  -1});
-            p.push_back({mp[a[i].s], i, 1});
-        }
-        sort(p.begin(), p.end());
-        for(auto e:p){
-            ll i=e.i, lft = mp[a[i].f], rit = mp[a[i].s];
-            if(e.v<0){
-                line++; update(lft, rit, -1, 1);
-            }
-            else
-            {
-                line--; update(lft, rit, 1, 1);
+        for(int j=0; j<p.size(); j++){
+            for(int i: lft[j]){
+                line++; update(a[i].f, a[i].s, -1, 1);
             }
             ans = max(ans, line+seg[1].val);
-            
+            for(int i: rit[j]){
+                line--; update(a[i].f, a[i].s, 1, 1);
+            }
         }
-        printf("Case %lld: %lld\n", t, ans);
+        printf("Case %d: %d\n", t, ans);
     }
     return 0;
     
